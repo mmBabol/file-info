e5c35102dcce280b937b43239625d4ac39997d5f
mmbabol <mmbabol@gmail.com>
2017-11-29 00:46:54 -0500

    New FileName, Sha1, and FileSize files. Removed node_modules directory

---
+++ src/FileSize.js
@@ -0,0 +1,14 @@
+var sizeof = require('object-sizeof');  // npm install object-sizeof
+let myFile;
+
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+  myFile = process.argv[2];
+}
+
+// If no arguments are entered, use file front sample directory
+if (process.argv.length = 2) {
+	myFile = "sample/sample.txt";
+}
+
+console.log("This file has " + sizeof(myFile) + " bytes.");
---
+++ src/Sha1Digest.js
@@ -0,0 +1,20 @@
+var sha1 = require('js-sha1');  // npm install js-sha1
+var myFile;
+
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+  myFile = process.argv[2];
+}
+
+// If no arguments are entered, use file front sample directory
+if (process.argv.length = 2) {
+	myFile = "sample/sample.txt";
+}
+
+// Read the file and print its contents.
+var fs = require('fs'), filename = myFile;
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  // Print SHA1 hash to of text file to screen
+  console.log(sha1(data))
+});
