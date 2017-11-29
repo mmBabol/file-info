c720513e6c607d232c270be73dcbc95398628eba
mmbabol <mmbabol@gmail.com>
2017-11-28 23:49:52 -0500

    FileName complete

--- src/file-info.js
+++ src/file-info.js
@@ -1,24 +1,33 @@
+//var sha1 = require('sha1');
+
 function getFileName() {
-	
+	let myName = document.getElementById('FileName');
+
+	myName.addEventListener('change', function(){
+		alert("The name of the file is" + this.files[0].name);
+	});
 }
 
 function getFileSize(){
-    
-    //Get the file by ID from the HTML file
-    var myFile = document.getElementById('FileSize');
-    
-    myFile.addEventListener('change', function() {
-   
-        alert("This file has " + this.files[0].size + " bytes.");
+	//Get the file by ID from the HTML file
+  var myFile = document.getElementById('FileSize');
 
-    }
-  );
+  myFile.addEventListener('change', function() {
+      alert("This file has " + this.files[0].size + " bytes.");
+  });
 }
 
 function getSha1Digest(){
-	
+
+	let mySha = document.getElementById('Sha1Digest');
+
+	mySha.addEventListener('change', function(){
+		alert(sha1(this.files[0].name));
+	});
+
+
 }
 
 function getMD5Digest() {
-	
+	alert("fuck me");
 }
